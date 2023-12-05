358547eff32a20687c4b5b6f45905239b6f2438c
Ficko0 <ficozlatanovski@gmail.com>
2023-12-05 17:33:30 +0100

    domashna krajna za html i css filip zlatanovski

---
+++ Basic JS Homeworks/basic javasScript homework 1/script.js
@@ -0,0 +1,14 @@
+console.log("zdravo svetu")
+
+let phonePrice = 119.95;
+let onePhonePriceWithTax = 125.95;
+let taxRate = 5.99;
+let numberOfPhones = 30;
+
+let priceOfPhonesWithTax = onePhonePriceWithTax * numberOfPhones;
+console.log(priceOfPhonesWithTax);
+
+
+
+
+
